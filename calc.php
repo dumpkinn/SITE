a48463c649f247b222e0e694fda749a49146a1e3
Sapinns <fenomcout@gmail.com>
2024-02-11 20:19:12 +0000

    calcupdate

--- calc.php
+++ calc.php
@@ -18,7 +18,12 @@ $fq1= $_GET['fq10'];
 $fq2= $_GET['fq11'];
 $bi1= $_GET['bi10'];
 $bi2= $_GET['bi11'];
+$ex1= $_GET['ex1'];
+$ex2= $_GET['ex2'];
+$ex3= $_GET['ex3'];
+$medex= $ex1+$ex2+$ex3;
 $med= ((($mat1+$mat2+$mat3)/3)+(($por1+$por2+$por3)/3)+(($edf1+$edf2+$edf3)/3)+(($fq1+$fq2)/2)+(($bi1+$bi2)/2)+(($fil1+$fil2)/2)+(($ing1+$ing2)/2)+$op1+$op2)/9;
+$fmedia= ($med+$medex)/2;
 
 
 
