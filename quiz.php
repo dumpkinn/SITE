ee444c678d1cba9cd5118341bbcc4251251f7a3f
Sapinns <fenomcout@gmail.com>
2024-04-02 02:13:38 +0100

    .

---
+++ quiz.php
@@ -0,0 +1,38 @@
+<?php
+$q1=$_GET['q1'];
+$q2=$_GET['q2'];
+$q3=$_GET['q3'];
+$q4=$_GET['q4'];
+$q5=$_GET['q5'];
+$q6=$_GET['q6'];
+$q7=$_GET['q7'];
+$q8=$_GET['q8'];
+$q9=$_GET['q9'];
+$q10=$_GET['q10'];
+$q11=$_GET['q11'];
+$q12=$_GET['q12'];
+$q13=$_GET['q13'];
+$q14=$_GET['q14'];
+$q15=$_GET['q15'];
+$score=$q1+$q2+$q3+$q4+$q5+$q6+$q7+$q8+$q9+$q10+$q11+$q12+$q13+$q14+$q15;
+
+if (-60<=$score<=-40) {
+    echo("Artistico");
+}
+elseif (-40<$score<=-20) {
+    echo("Social");
+}
+elseif (-20<$score<=0) {
+    echo("Enterprising");
+}
+elseif (0<$score<=20) {
+    echo("Convencional");
+}
+
+elseif (20<$score<=40) {
+    echo("Realista");
+}
+elseif (40<$score<=60) {
+    echo("Investigação");
+}
+?>
