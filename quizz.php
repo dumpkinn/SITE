8ec9499fcb768d86e624cad4a7742f451fb5b952
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-04 01:16:44 +0100

    sdfsdaf
    afdsafdsafds
    asdfa

--- quizz.php
+++ quizz.php
@@ -650,7 +650,6 @@ function closePopup() {
         /* Popup content */
         .popup-content {
             background-color: #fefefe;
-            margin: 15% auto; /* 15% from the top and centered */
             padding: 20px;
             border: 1px solid #888;
             width: 80%; /* Could be more or less, depending on screen size */
