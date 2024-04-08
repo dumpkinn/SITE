89e4dc05f132a4e2c7525f0d8ea04039a362ccdf
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-08 23:54:44 +0100

    Update curriculo.php

--- curriculo.php
+++ curriculo.php
@@ -52,6 +52,106 @@
         </div>
     </div>
   </header>
+    <div class="page-heading header-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <span class="breadcrumb">Testa / O Currículo</span>
+          <h3>Currículo</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+<div class="contact-content">
+  <div class="single-property section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="main-image">
+            <form action="" id="contact-form">
+              <div class="col-lg-12">
+                <fieldset>
+                  <label for="message">Introduz o teu currículo</label><br>
+                  <textarea name="message" id="message" placeholder="O teu currículo..."></textarea>
+                </fieldset>
+              </div>
+              <div class="col-lg-12">
+                <fieldset>
+                  <button type="submit" id="form-submit" class="orange-button">Testa o teu currículo</button>
+                </fieldset>
+              </div>
+            </form>
+          </div>
+          <div class="main-content">
+          </div> 
+          <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Como é que funciona?
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  Introduz o teu currículo na caixa de texto acima, depois de submeteres a nossa <strong>Inteligência artificial</strong> irá avaliar o teu currículo em diferentes parâmetros. Quando o teu currículo for avaliado, ele irá receber uma pontuação de um a cinco e uma e serão descritos os pontos fracos, de modo a melhorar a qualidade do currículo 
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Como é que foi feita?
+                </button>
+              </h2>
+              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                   A nossa <strong>inteligência artificial</strong> recebeu indicações iniciais como procurar por erros gramaticais, procurar por palávras chave que enriquecem o curricúlo, depois esta foi submetida a milhares de testes para avaliar currículos, estes currículos foram pré-avaliados por nós. No final, esta já conseguia obter resultados satisfatórios. 
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Qual é a utilidade desta ferramenta?
+                </button>
+              </h2>
+              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  Cada vez mais a <strong>inteligência artificial</strong> tem sido utilizada para escolher os currículos mais relevantes nas empresas que recebem milhares de candidaturas de emprego. Assim, ao criarmos o nosso próprio sistema é possível ter uma ideia de o que importa num currículo, bem como o que é que um computador procura num currículo ideal.
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="info-table">
+            <ul>
+              <li>
+                <img src="assets/images/info-icon-01.png" alt="" style="max-width: 52px;">
+                <h4>450 m2<br><span>Total Flat Space</span></h4>
+              </li>
+              <li>
+                <img src="assets/images/info-icon-02.png" alt="" style="max-width: 52px;">
+                <h4>Contract<br><span>Contract Ready</span></h4>
+              </li>
+              <li>
+                <img src="assets/images/info-icon-03.png" alt="" style="max-width: 52px;">
+                <h4>Payment<br><span>Payment Process</span></h4>
+              </li>
+              <li>
+                <img src="assets/images/info-icon-04.png" alt="" style="max-width: 52px;">
+                <h4>Safety<br><span>24/7 Under Control</span></h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
   <footer>
     <div class="container">
       <div class="col-lg-8">
