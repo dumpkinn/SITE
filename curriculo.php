134e5e71c07340e9631e87747e36e7e695774610
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-08 23:54:00 +0100

    Update curriculo.php

--- curriculo.php
+++ curriculo.php
@@ -52,13 +52,6 @@
         </div>
     </div>
   </header>
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/counter.js"></script>
-  <script src="assets/js/custom.js"></script>
-
   <footer>
     <div class="container">
       <div class="col-lg-8">
