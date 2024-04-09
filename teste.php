78a3e16e488c7bc7f39bb6cc889fed10dc3f994e
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-09 21:03:06 +0100

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-  <?php
-  session_start();
-  ?>
 
   <head>
     <style>
@@ -13,24 +10,7 @@
             color: red;
         }
     </style>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <title>Rumo ao Futuro</title>
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-villa-agency.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
-  </head>
 <body>
-      <div class="row properties-box">
-        <div class="col-lg-4 col-md-6 align-self-center mb-30 properties-items col-md-6 adv">
-          <div class="item">
-            <a href="property-details.html"><img src="cursosimg/Engenharia Informática.png" alt=""></a>
-            <span class="category">IS. Técnico</span>
-            <h6>18.1</h6>
               <span id="textToHighlight">
         <h4>Engenharia Informática</h4>
     </span>
