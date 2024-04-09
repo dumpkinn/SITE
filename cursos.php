de8b3790e70f26bd0009d80bbaf5f36a640ba27d
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-09 01:31:45 +0100

    Update cursos.php

--- cursos.php
+++ cursos.php
@@ -98,7 +98,23 @@
           <a href="#!" data-filter=".con">Convencional</a>
         </li>
       </ul>
-          <script>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+      <div class="row properties-box">
+        <div class="col-lg-4 col-md-6 align-self-center mb-30 properties-items col-md-6 adv">
+          <div class="item">
+            <a href="property-details.html"><img src="cursosimg/Engenharia Informática.png" alt=""></a>
+            <span class="category">IS. Técnico</span>
+            <h6>18.1</h6>
+            <span id="textToHighlight">
+        <h4>Engenharia Informática</h4>
+    </span>
+
+    <script>
         var s = 20;
         textElement.textContent = s;
 
@@ -113,13 +129,6 @@
            
         }
     </script>
-
-            <img src="cursosimg/Engenharia Informática.png" alt="">
-            <span class="category">IS. Técnico</span>
-            <h6>18.1</h6>
-            <span id="textToHighlight">
-        Engenharia Informática
-    </span>
             <div class="main-button">
               <a href="https://tecnico.ulisboa.pt/pt/ensino/cursos/licenciaturas/engenharia-informatica-e-de-computadores/">Mais informação</a>
             </div>
