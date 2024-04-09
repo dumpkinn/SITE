d9c75571679bb2202f5831aae2b384cc3f5e38c9
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-09 01:26:43 +0100

    Update cursos.php

--- cursos.php
+++ cursos.php
@@ -98,9 +98,7 @@
           <a href="#!" data-filter=".con">Convencional</a>
         </li>
       </ul>
-      <div class="row properties-box">
-        <div class="col-lg-4 col-md-6 align-self-center mb-30 properties-items col-md-6 adv">
-          <div class="item">
+
             <a href="property-details.html"><img src="cursosimg/Engenharia Informática.png" alt=""></a>
             <span class="category">IS. Técnico</span>
             <h6>18.1</h6>
