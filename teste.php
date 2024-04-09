37e4977df2705ad087446675eb48ef71323684b2
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-09 20:52:40 +0100

    Update teste.php

--- teste.php
+++ teste.php
@@ -14,12 +14,7 @@
         }
     </style>
 
-      <div class="row properties-box">
-        <div class="col-lg-4 col-md-6 align-self-center mb-30 properties-items col-md-6 adv">
-          <div class="item">
-            <a href="property-details.html"><img src="cursosimg/Engenharia Informática.png" alt=""></a>
-            <span class="category">IS. Técnico</span>
-            <h6>18.1</h6>
+
             <span id="textToHighlight">
         <h4>Engenharia Informática</h4>
     </span>
