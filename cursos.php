2e11b3ec598c4a13b6dd6538aaca15cb9b692039
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-12 19:41:25 +0100

    Update cursos.php

--- cursos.php
+++ cursos.php
@@ -14,6 +14,23 @@
         }
     </style>
     <meta charset="utf-8">
+    <script type='text/javascript'>
+
+(function()
+{
+  if( window.localStorage )
+  {
+    if( !localStorage.getItem('firstLoad') )
+    {
+      localStorage['firstLoad'] = true;
+      window.location.reload();
+    }  
+    else
+      localStorage.removeItem('firstLoad');
+  }
+})();
+
+</script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <title>Rumo ao Futuro</title>
