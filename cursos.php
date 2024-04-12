b1256dc3a20dd85aafc15c9f7fcc32f5a0aef1d2
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-12 19:44:04 +0100

    Update cursos.php

--- cursos.php
+++ cursos.php
@@ -15,17 +15,12 @@
     </style>
     <meta charset="utf-8">
     <script type="text/javascript">
-$(document).ready(function(){    
-    //Check if the current URL contains '#'
-    if(document.URL.indexOf("#")==-1){
-        // Set the URL to whatever it was plus "#".
-        url = document.URL+"#";
-        location = "#";
-
-        //Reload the page
-        location.reload(true);
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
     }
-});
+}
 </script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
