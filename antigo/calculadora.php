eb65de453e01eedc27d9944d001f9abb474ebe5d
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-16 22:28:03 +0100

    Update calculadora.php

--- antigo/calculadora.php
+++ antigo/calculadora.php
@@ -6,17 +6,6 @@ session_start();
 
   <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <title>Rumo ao Futuro</title>
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-villa-agency.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
-    <link rel="icon" href="icon.png" type="image/gif" sizes="16x16">
   </head>
 <body>
       </div>
