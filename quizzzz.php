fbf045ed29586477b7e316c130a0cbb9dd159939
dumpkinn <81766750+dumpkinn@users.noreply.github.com>
2024-04-19 23:42:04 +0100

    dsf

--- quizzzz.php
+++ quizzzz.php
@@ -625,7 +625,7 @@ button:hover {
 												</div>
 												<div class="swiper-slide">
 													<h1 class="vrmh">Questão 10</h1><br>
-<div class="img_div"><figcaption>Décnico dentário</figcaption><img src="quizimg/q101.png"><br><br>
+<div class="img_div"><figcaption>Mécnico dentário</figcaption><img src="quizimg/q101.png"><br><br>
   <div class="cc-selector">
   <input type="radio" name="q10" id="visa37" value="2"/>
   <label class="drinkcard-cc visa"  for="visa37"></label>
