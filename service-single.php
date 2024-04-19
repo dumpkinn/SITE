fd237c6d7054eef30076d57e56f2a77e7b812463
VALEILO <132665721+VALEILO@users.noreply.github.com>
2024-04-19 14:46:24 +0100

    Update service-single.php

--- service-single.php
+++ service-single.php
@@ -80,6 +80,64 @@
 							<!--Navigation-->
 							<!--Menu-->
 							<ul class="sf-menu" data-back-text="Back">
+								<li ><a href="index.html"><span>Home</span></a>
+									
+								</li>
+								<li ><a href="blog-grid-4-col.php"><span>Cursos</span></a></li>
+									
+								<li ><a href="service-single.php"><span>Calculadora</span></a></li>
+								<li ><a href="quizzzz.php"><span>Quiz</span></a></li>
+								<li ><a href="blog-grid-1-col.html"><span>Mapa</span></a></li>
+								<li ><a href="blog-grid-1-col.html"><span>Curriculo</span></a></li>
+									
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+	<div class="d-lg-none d-sm-block">
+		<header class="vlt-header vlt-header--fullscreen">
+			<div class="vlt-navbar vlt-navbar--main vlt-navbar--transparent vlt-navbar--sticky vlt-navbar--white-text-on-top vlt-navbar--transparent vlt-navbar--sticky vlt-navbar--white-text-on-top">
+				<div class="container">
+					<div class="vlt-navbar-inner">
+						<div class="vlt-navbar-inner--left">
+							<div class="d-flex align-items-center justify-content-center h-100">
+								<!--Logo--><a class="vlt-navbar-logo" href="index.html"><img class="black" src="assets/img/root/logo-black.png" alt="" style="height: 25px;" loading="lazy"><img class="white" src="assets/img/root/logo-white.png" alt="" style="height: 25px;" loading="lazy"></a>
+							</div>
+						</div>
+						<div class="vlt-navbar-inner--right">
+							<div class="d-flex align-items-center justify-content-center h-100">
+								<!--Buttons-->
+								<div class="vlt-navbar-buttons d-flex align-items-center"><a class="vlt-menu-burger js-fullscreen-menu-toggle" href="#"><span class="line-one"></span><span class="line-two"></span></a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<nav class="vlt-nav vlt-nav--fullscreen" data-submenu-effect="style-1">
+			<div class="vlt-nav--fullscreen__mask"><svg xmlns="http://www.w3.org/2000/svg" width="895" height="267">
+					<path fill="none" fill-rule="nonzero" stroke="currentColor" stroke-width="2" d="M376.98 63.08c22.08 0 39.72 4.8 52.92 14.4 13.2 9.6 22.68 22.44 28.44 38.52 5.76 16.08 8.64 33.6 8.64 52.56h0v9H334.86c0 15.36 4.08 27 12.24 34.92 7.92 8.16 19.08 12.24 33.48 12.24 7.44 0 14.28-2.04 20.52-6.12s10.2-9 11.88-14.76h51.84c-4.08 13.44-10.08 24.48-18 33.12-8.16 9.12-17.88 16.08-29.16 20.88-11.76 4.8-24.12 7.2-37.08 7.2-63.84 0-95.76-34.92-95.76-104.76 0-16.56 3.12-32.4 9.36-47.52 6.24-15.12 16.32-27.12 30.24-36 14.16-9.12 31.68-13.68 52.56-13.68zM81.06 1.16l46.44 204.12L174.66 1.16h78.12V260h-50.76V43.28l-.72.72-47.88 216h-53.28L52.98 43.28l-.72.72v216H1.5V1.16h79.56zm295.2 103.32c-22.56 0-35.64 13.68-39.24 41.04h78.12c-.96-12.24-5.04-22.32-12.24-30.24-7.2-7.2-16.08-10.8-26.64-10.8zM550.86 260V150.2c0-12 3.12-22.44 9.36-31.32 6.24-8.64 16.2-12.96 29.88-12.96 14.4 0 23.52 3.36 27.36 10.08 3.84 6.72 5.76 16.08 5.76 28.08V260h50.76V127.16c0-13.44-2.64-24.96-7.92-34.56-5.28-9.36-12.96-16.68-23.04-21.96-10.32-5.04-22.32-7.56-36-7.56-25.92 0-45.36 11.04-58.32 33.12V68.12h-48.6V260h50.76zm235.44 5.04c12.96 0 24.24-2.64 33.84-7.92 9.6-5.28 18-13.2 25.2-23.76V260h47.88V68.12h-50.04V179c0 10.56-1.92 18.96-5.76 25.2-3.84 6.72-8.88 11.4-15.12 14.04-6.48 2.88-12.6 4.32-18.36 4.32-21.6 0-32.4-11.88-32.4-35.64V68.12h-50.76v126.36c0 23.04 6 40.56 18 52.56s27.84 18 47.52 18z" />
+				</svg>
+			</div>
+			<div class="vlt-nav-table">
+				<div class="vlt-nav-row">
+					<div class="vlt-nav--fullscreen__header">
+						<div class="container">
+							<div class="d-flex align-items-center justify-content-between">
+								<!--Logo--><a class="vlt-navbar-logo" href="index.html"><img src="assets/img/root/logo-black.png" alt="" style="height: 25px;" loading="lazy"></a><a class="vlt-menu-burger js-fullscreen-menu-toggle" href="#"><span class="line-one"></span><span class="line-two"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="vlt-nav-row vlt-nav-row--full vlt-nav-row--center">
+					<div class="container">
+						<div class="vlt-nav--fullscreen__navigation">
+							<!--Navigation-->
+							<!--Menu-->
+							<ul class="sf-menu" data-back-text="Back">
 								<li class="menu-item-has-children"><a href="index.html"><span>Home</span></a>
 									
 								</li>
