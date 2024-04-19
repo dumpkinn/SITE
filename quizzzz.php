d4a275a32ad981d6c8aa1ab69c986939e1ef293d
VALEILO <132665721+VALEILO@users.noreply.github.com>
2024-04-19 14:43:27 +0100

    Update quizzzz.php

--- quizzzz.php
+++ quizzzz.php
@@ -1,7 +1,17 @@
-<!DOCTYPE html>
-<html class="no-js vlt-is--service-single vlt-is--header-fullscreen vlt-is--footer-style-1 vlt-is--footer-fixed" lang="en">
-<?php session_start(); ?>
+	<!DOCTYPE html>
+<html class="no-js vlt-is--blog-grid-4-col vlt-is--header-fullscreen vlt-is--footer-style-1 vlt-is--footer-fixed" lang="en">
+<?php
+session_start();
+?>
 <head>
+<style>
+        .gr{
+            color: green;
+        }
+        .re{
+            color: red;
+        }
+    </style>
 	<meta charset="utf-8">
 	<title>Quiz</title>
 	<meta name="description" content="Creative &amp; Modern Agency HTML5 Template">
@@ -21,86 +31,6 @@
 	<link rel="stylesheet" href="assets/css/vlt-main.min.css">
 	<!--Custom-->
 	<link rel="stylesheet" href="assets/css/custom.css">
-    <style type="text/css">
-    button {
-    display: block;
-    padding: 10px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-    transition: background-color 0.3s;
-    width: 100%;
-}
-.container3 img{
-  width: 200px;
-
-}
-.container3 figcaption{
-  text-align: center;
-}
-.container3{
-  width: 850px;
-  margin: auto;
-}
-
-button:hover {
-    background-color: #0056b3;
-}
-        .img_div { float: left; }
-        [type=radio] { 
-  position: absolute;
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-[type=radio] img {
-  cursor: pointer;
-}
-[type=radio]:checked img {
-  outline: 2px solid #f00;
-}
-.cc-selector input{
-    margin:0;padding:0;
-    -webkit-appearance:none;
-       -moz-appearance:none;
-            appearance:none;
-}
-.cc-selector-2 input{
-    position:absolute;
-    z-index:999;
-}
-.cc-selector-2 input:active +.drinkcard-cc, .cc-selector input:active +.drinkcard-cc{opacity: .9;}
-.cc-selector-2 input:checked +.drinkcard-cc, .cc-selector input:checked +.drinkcard-cc{
-    -webkit-filter: none;
-       -moz-filter: none;
-            filter: none;
-}
-.cc-selector-2 input{ margin: 5px 0 0 12px; }
-.cc-selector-2 label{ margin-left: 7px; }
-.drinkcard-cc{
-    cursor:pointer;
-    background-size:contain;
-    background-repeat:no-repeat;
-    display:inline-block;
-    width:100px;height:70px;
-    -webkit-transition: all 100ms ease-in;
-       -moz-transition: all 100ms ease-in;
-            transition: all 100ms ease-in;
-    -webkit-filter: brightness(1.8) grayscale(1) opacity(.7);
-       -moz-filter: brightness(1.8) grayscale(1) opacity(.7);
-            filter: brightness(1.8) grayscale(1) opacity(.7);
-}
-.drinkcard-cc:hover{
-    -webkit-filter: brightness(1.2) grayscale(.5) opacity(.9);
-       -moz-filter: brightness(1.2) grayscale(.5) opacity(.9);
-            filter: brightness(1.2) grayscale(.5) opacity(.9);
-}
-.visa{background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3_-obTn0DZXkN1gEqr15R0Od_Icndc__lKLE88hBjjg&s);}
-.mastercard{background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq6HyM_SlUH34PMKesxgDd3WTMh_db7rvsD6r7Uisf1Q&s);}
-    </style>
 </head>
 
 <body>
@@ -159,7 +89,7 @@ button:hover {
 						<div class="vlt-nav--fullscreen__navigation">
 							<!--Navigation-->
 							<!--Menu-->
-<ul class="sf-menu" data-back-text="Back">
+							<ul class="sf-menu" data-back-text="Back">
 								<li ><a href="index.html"><span>Home</span></a>
 									
 								</li>
@@ -184,6 +114,67 @@ button:hover {
 					<div class="vlt-navbar-inner">
 						<div class="vlt-navbar-inner--left">
 							<div class="d-flex align-items-center justify-content-center h-100">
+								<!--Logo--><a class="vlt-navbar-logo" href="index.html"><img class="black" src="assets/img/root/logo-black.png" alt="" style="height: 25px;" loading="lazy"><img class="white" src="assets/img/root/logo-white.png" alt="" style="height: 25px;" loading="lazy"></a>
+							</div>
+						</div>
+						<div class="vlt-navbar-inner--right">
+							<div class="d-flex align-items-center justify-content-center h-100">
+								<!--Buttons-->
+								<div class="vlt-navbar-buttons d-flex align-items-center"><a class="vlt-menu-burger js-fullscreen-menu-toggle" href="#"><span class="line-one"></span><span class="line-two"></span></a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<nav class="vlt-nav vlt-nav--fullscreen" data-submenu-effect="style-1">
+			<div class="vlt-nav--fullscreen__mask"><svg xmlns="http://www.w3.org/2000/svg" width="895" height="267">
+					<path fill="none" fill-rule="nonzero" stroke="currentColor" stroke-width="2" d="M376.98 63.08c22.08 0 39.72 4.8 52.92 14.4 13.2 9.6 22.68 22.44 28.44 38.52 5.76 16.08 8.64 33.6 8.64 52.56h0v9H334.86c0 15.36 4.08 27 12.24 34.92 7.92 8.16 19.08 12.24 33.48 12.24 7.44 0 14.28-2.04 20.52-6.12s10.2-9 11.88-14.76h51.84c-4.08 13.44-10.08 24.48-18 33.12-8.16 9.12-17.88 16.08-29.16 20.88-11.76 4.8-24.12 7.2-37.08 7.2-63.84 0-95.76-34.92-95.76-104.76 0-16.56 3.12-32.4 9.36-47.52 6.24-15.12 16.32-27.12 30.24-36 14.16-9.12 31.68-13.68 52.56-13.68zM81.06 1.16l46.44 204.12L174.66 1.16h78.12V260h-50.76V43.28l-.72.72-47.88 216h-53.28L52.98 43.28l-.72.72v216H1.5V1.16h79.56zm295.2 103.32c-22.56 0-35.64 13.68-39.24 41.04h78.12c-.96-12.24-5.04-22.32-12.24-30.24-7.2-7.2-16.08-10.8-26.64-10.8zM550.86 260V150.2c0-12 3.12-22.44 9.36-31.32 6.24-8.64 16.2-12.96 29.88-12.96 14.4 0 23.52 3.36 27.36 10.08 3.84 6.72 5.76 16.08 5.76 28.08V260h50.76V127.16c0-13.44-2.64-24.96-7.92-34.56-5.28-9.36-12.96-16.68-23.04-21.96-10.32-5.04-22.32-7.56-36-7.56-25.92 0-45.36 11.04-58.32 33.12V68.12h-48.6V260h50.76zm235.44 5.04c12.96 0 24.24-2.64 33.84-7.92 9.6-5.28 18-13.2 25.2-23.76V260h47.88V68.12h-50.04V179c0 10.56-1.92 18.96-5.76 25.2-3.84 6.72-8.88 11.4-15.12 14.04-6.48 2.88-12.6 4.32-18.36 4.32-21.6 0-32.4-11.88-32.4-35.64V68.12h-50.76v126.36c0 23.04 6 40.56 18 52.56s27.84 18 47.52 18z" />
+				</svg>
+			</div>
+			<div class="vlt-nav-table">
+				<div class="vlt-nav-row">
+					<div class="vlt-nav--fullscreen__header">
+						<div class="container">
+							<div class="d-flex align-items-center justify-content-between">
+								<!--Logo--><a class="vlt-navbar-logo" href="index.html"><img src="assets/img/root/logo-black.png" alt="" style="height: 25px;" loading="lazy"></a><a class="vlt-menu-burger js-fullscreen-menu-toggle" href="#"><span class="line-one"></span><span class="line-two"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="vlt-nav-row vlt-nav-row--full vlt-nav-row--center">
+					<div class="container">
+						<div class="vlt-nav--fullscreen__navigation">
+							<!--Navigation-->
+							<!--Menu-->
+							<ul class="sf-menu" data-back-text="Back">
+								<li class="menu-item-has-children"><a href="index.html"><span>Home</span></a>
+									
+								</li>
+								<li class="menu-item-has-children"><a href="work-asymmetric-01.html"><span>Cursos</span></a>
+									 
+								</li>
+								<li class="menu-item-has-children left"><a href="about-us.html"><span>Calculadora</span></a>
+									
+								</li>
+								<li class="menu-item-has-children left"><a href="quizzzz.php"><span>Quiz</span></a>
+									
+								<li class="menu-item-has-children left"><a href="contact-01.html"><span>Mapa</span></a>
+									
+								</li>
+								<li class="menu-item-has-children left"><a href="contact-01.html"><span>Currículo</span></a>
+									
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="vlt-nav-row">
+					<div class="vlt-gap-60"></div>
+				</div>
+			</div>
+		</nav>
+	</div>
 	<!--Site overlay-->
 	<div class="vlt-site-overlay"></div>
 	<!--Main-->
