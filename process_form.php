e3c506f24315a7aaaef1dd017d49f96700e9af01
VALEILO <132665721+VALEILO@users.noreply.github.com>
2024-04-30 11:29:07 +0100

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,13 +1,17 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $company = $_POST['company'];
-    $title = $_POST['title'];
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //$name = $_POST['name'];
+    //$email = $_POST['email'];
+    //$phone = $_POST['phone'];
+    //$company = $_POST['company'];
+    //$title = $_POST['title'];
     $message = $_POST['message'];
 
     $to = "codeecrusaders@gmail.com";
     $subject = "New Contact Form Submission";
-    $body = "Name: $name\nEmail: $email\nPhone: $phone\nCompany: $company\nTitle: $title\nMessage: $message";
+    
+    mail($to,$subject,$message)
+    //$body = "Name: $name\nEmail: $email\nPhone: $phone\nCompany: $company\nTitle: $title\nMessage: $message";
+
+
 ?>
