89d6f75238f122291f1d71ea38179c97b44ade11
VALEILO <132665721+VALEILO@users.noreply.github.com>
2024-04-30 12:26:00 +0100

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,21 +1,12 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $company = $_POST['company'];
-    $title = $_POST['title'] ;
-    $message = $_POST['message'];
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //$name = $_POST['name'];
+    //$email = $_POST['email'];
+    //$phone = $_POST['phone'];
+    //$company = $_POST['company'];
+    //$title = $_POST['title'];
+    $message = "teste";
     $to = "codeecrusaders@gmail.com";
     $subject = "New Contact Form Submission";
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-    $email_body = "<h2>New Contact Form Submission</h2>";
-    $email_body .= "<p><strong>Name:</strong> $name</p>";
-    $email_body .= "<p><strong>Email:</strong> $email</p>";
-    $email_body .= "<p><strong>Phone:</strong> $phone</p>";
-    $email_body .= "<p><strong>Company:</strong> $company</p>";
-    $email_body .= "<p><strong>Title:</strong> $title</p>";
-    $email_body .= "<p><strong>Message:</strong> $message</p>";
-mail($to, $subject, $email_body, $headers)
+    mail($to,$subject,$message);
 ?>
